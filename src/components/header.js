9de69be3019733d2808801cd2e4a0d1ee21737a3
Đivo Zec <dzec01@gmail.com>
2017-11-06 19:43:09 +0100

    Made style changes to header component, created AlbumList

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,35 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 
 // Make a component
-const Header = () => {
-    return <Text> Albums </Text>;
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    return (
+    <View style={viewStyle}>
+        <Text style={textStyle}> {props.headerText} </Text>
+    </View>
+    );
 };
 
+const styles = {
+    viewStyle: {
+        justifyContent: 'center',
+        backgroundColor: '#F1F1F1',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',                    //iOS only
+        shadowOffset: { width: 0, height: 2 },  //iOS only
+        shadowOpacity: 0.2,                     //iOS only
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
 
 // Make component available for other parts of the App
 export default Header;
