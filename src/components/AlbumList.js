8294bbfba1cfc23eb51fa92e1f63fa8f3aa80bfa
Đivo Zec <dzec01@gmail.com>
2017-11-07 11:50:41 +0100

    Made AlbumList a class component.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, } from 'react-native';
 
-const AlbumList = () => {
+class AlbumList extends Component {
+    render() {
     return (
         <View> 
             <Text>Album list</Text>     
         </View>
     );
-};
-
+    }
+}
 export default AlbumList;
